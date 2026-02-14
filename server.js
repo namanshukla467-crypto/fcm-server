fe99f1d664ec6f566b8b82e7a63c69d3a769c602
namanshukla467-crypto <namanshukla467@gmail.com>
2026-02-15 01:55:31 +0530

    Refactor Firebase initialization with env variables
    Updated Firebase initialization to use environment variables for credentials.

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./serviceAccountKey.json");
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: "globalchat-51d94",
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
 });
 
 const app = express();
