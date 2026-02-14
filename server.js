80d329768d8a7ee87c1dd88258bffb1d00c3422a
namanshukla467-crypto <namanshukla467@gmail.com>
2026-02-15 02:10:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,6 +30,10 @@ app.post("/send", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server running");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
+
+
