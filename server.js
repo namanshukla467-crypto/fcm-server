068af7b338b596b14edc19ffb14d8d9ab812c810
namanshukla467-crypto <namanshukla467@gmail.com>
2026-02-15 19:03:09 +0530

    Add health check endpoint
    Added a health check endpoint to respond with 'Server Alive'.

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ admin.initializeApp({
 const app = express();
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Server Alive");
+});
+
 app.post("/send", async (req, res) => {
   const { token, title, body } = req.body;
 
@@ -35,5 +39,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
+
 
 
